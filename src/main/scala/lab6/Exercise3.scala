a964b2eb837114f9d1cc3eb0eb43ba83dddaacfe
Muhammad Shahzaib <mohammadshahzaib2028@gmail.com>
2021-04-08 10:35:40 +0500

    Update Exercise3.scala

--- src/main/scala/lab6/Exercise3.scala
+++ src/main/scala/lab6/Exercise3.scala
@@ -2,21 +2,24 @@ package lab6
 import chisel3._
 import chisel3.util._
 
-class Exercise3( val din : Int ) extends Module {
+class Exercise3( val din : Int) extends Module {
     val io = IO ( new Bundle {
     val reload = Output (Bool())
     })
-    val counter = RegInit (din. U (4.W ) )
-    val done = counter === 0. U
+    val counter1 = RegInit (din.U (4.W))
+    val counter2 = RegInit (din.U (4.W))
+    val done = counter1 === 0.U  && counter2 === 0.U
     val next = WireInit(0.U)
-    counter:=next
+    counter1 := next
+    counter2 := next
     io.reload:=0.B
     when (done){
 	 io.reload := 1.B
     }.elsewhen(!done){
-        next:=counter - 1.U
+        next:= counter1 - 1.U
+	next := counter2 - 1.U
          io.reload:=0.B
-    }.elsewhen(counter === 0.U){
+    }.elsewhen(counter1 === 0.U && counter2 === 0.U){
     	io.reload := 1.B
     }
 
